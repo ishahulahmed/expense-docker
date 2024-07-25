070bee8012641a983b3165ce615e295f241e9626
ishahulahmed <shahulahmed1717@gmail.com>
2024-07-25 12:39:19 +0530

    expense-docker

--- install-docker.sh
+++ install-docker.sh
@@ -3,8 +3,9 @@
 # check whether root user or not
 R="\e[31m"
 N="\e[0m"
+
 yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yes | yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl start docker
 systemctl status docker
