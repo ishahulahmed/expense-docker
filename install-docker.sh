6a1b608ca1900d33c77215e0639484a4367fe20e
ishahulahmed <shahulahmed1717@gmail.com>
2024-07-25 13:39:06 +0530

    expense-docker

--- install-docker.sh
+++ install-docker.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 
 yum install -y yum-utils
 yum-config-manager -y --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
